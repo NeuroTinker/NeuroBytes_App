41f3d1a246541996790aaa04f077ea60a83baa43
jrwhite <jrwhite20@gmail.com>
2018-05-25 13:39:51 -0500

    initial support for OpenGL ES plotting

---
+++ app/src/main/java/com/neurotinker/neurobytes/RtPlot.java
@@ -0,0 +1,11 @@
+package com.neurotinker.neurobytes;
+
+public class RtPlot {
+    /**
+     * Controller for real-time neuron plotting
+     *
+     * Wraps the plot-lib OpenGL ES Library
+     */
+
+
+}
